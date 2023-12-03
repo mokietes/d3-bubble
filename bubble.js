dfb9045dc826715f9bb1183dcc2b234deaabf7aa
Mekonnen T Tesfazien <102988387+mokietes@users.noreply.github.com>
2023-12-02 21:46:09 -0800

    update

--- bubble.js
+++ bubble.js
@@ -1,16 +1,24 @@
-const file = 'data.json';
+const file = 'newData.json';
 const width = window.innerWidth;
 const height = window.innerHeight;
 const colors = {
-    html: '#F16529',
-    css: '#1C88C7',
-    js: '#FCC700'
+    "Unintentional injuries": '#FF5733', // Bright orange
+    "All causes": '#FFC300', // Bright yellow
+    "Alzheimer's disease": '#2A84D2', // Bright orange
+    "Diabetes": '#FFE4B5', // Bright yellow
+    "CLRD": '#F08080', // Bright orange
+    "Stroke": '#2E9785', // Bright yellow
+    "Suicide": '#7274C6', // Bright orange
+    "Influenza and pneumonia": '#FFC300', // Bright yellow
+    "Cancer": '#FFC300', // Bright yellow
+    "Kidney disease": "#FF7F50",
+    "Heart disease": "#E759A6"
 };
 
 const generateChart = data => {
     const bubble = data => d3.pack()
         .size([width, height])
-        .padding(2)(d3.hierarchy({ children: data }).sum(d => d.score));
+        .padding(2)(d3.hierarchy({ children: data }).sum(d => d.Age_adjusted_Death_Rate));
 
     const svg = d3.select('#bubble-chart')
         .style('width', width)
@@ -25,11 +33,11 @@ const generateChart = data => {
         .attr('transform', `translate(${width / 2}, ${height / 2})`);
     
     const circle = node.append('circle')
-        .style('fill', d => colors[d.data.category])
+        .style('fill', d => colors[d.data.Cause_Name])
         .on('mouseover', function (e, d) {
-            tooltip.select('img').attr('src', d.data.img);
-            tooltip.select('a').attr('href', d.data.link).text(d.data.name);
-            tooltip.select('span').attr('class', d.data.category).text(d.data.category);
+            //tooltip.select('a').attr('src', d.data.Deaths);
+            tooltip.select('a').text(d.data.Cause_Name);
+            tooltip.select('span').attr('class', d.data.Cause_Name).text(d.data.Cause_Name);
             tooltip.style('visibility', 'visible');
 
             d3.select(this).style('stroke', '#222');
@@ -40,11 +48,11 @@ const generateChart = data => {
             d3.select(this).style('stroke', 'none');
             return tooltip.style('visibility', 'hidden');
         })
-        .on('click', (e, d) => window.open(d.data.link));
+        // .on('click', (e, d) => window.open(d.data.link));
     
     const label = node.append('text')
         .attr('dy', 2)
-        .text(d => d.data.name.substring(0, d.r / 3));
+        .text(d => d.data.Cause_Name);
 
     node.transition()
         .ease(d3.easeExpInOut)
@@ -63,7 +71,15 @@ const generateChart = data => {
         .style('opacity', 1)
 };
 
+
+// Filter the data based on the selected year
+// const filterDataByYear = (data, selectedYear) => {
+//     return data.filter(d => d.Year === selectedYear );
+// };
+
 (async () => {
-    data = await d3.json(file).then(data => data);
+    const data = await d3.json(file).then(data => data.filter(d => d.Year === 2000 && d.State === "United States" && d.Cause_Name !== "All causes" ));
+    // const selectedYear = '2016'; // Replace with the desired year
+    // const filteredData = filterDataByYear(data, selectedYear);
     generateChart(data);
 })();
